7debba4bb189ea60e13054f5769035963ade45bb
Tomás Martins <tomas.marti@me.com>
2024-09-24 14:17:55 -0300

    Initial Commit

---
+++ ShazamKitBackground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShazamKitBackground
+//
+//  Created by Tomas Martins on 24/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShazamKitBackground/ShazamKitBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShazamKitBackgroundApp.swift
+//  ShazamKitBackground
+//
+//  Created by Tomas Martins on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ShazamKitBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
